be8ae7d7717f016bd1ce00bf321b599533141c53
Sarah Tavernel <sarahtavernel@MacBook-Pro-de-Sarah.local>
2018-12-27 12:31:55 +0100

    helloservlet

---
+++ src/com/eclipsetotale/HelloServlet.java
@@ -0,0 +1,50 @@
+package com.eclipsetotale;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet({ "/HelloServlet", "/hello" })
+public class HelloServlet extends HttpServlet implements Servlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String qui = request.getParameter("qui");
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/html");
+		out.println("<head><title>WTPproject</title></head>");
+		out.println("<body>");
+		out.print("Bonjour " + qui + "!");
+		out.println("</body>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
